3c9add04693c402816d82bf66bdc1e19e86f872d
mioha <mioha@localhost>
2026-09-25 16:53:26 +0800

    fix：修正 eza 别名参数解析

--- configuration/home/programs/Terminal/fish/dotfiles/config.fish
+++ configuration/home/programs/Terminal/fish/dotfiles/config.fish
@@ -2,10 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting ""
-alias ls='eza --icons'
-alias ll='eza -l --icons'
-alias la='eza -a --icons'
-alias l='eza -la --icons'
-alias lt='eza -T --icons'
-alias lta='eza -Ta --icons'
+alias ls='eza --icons=auto'
+alias ll='eza -l --icons=auto'
+alias la='eza -a --icons=auto'
+alias l='eza -la --icons=auto'
+alias lt='eza -T --icons=auto'
+alias lta='eza -Ta --icons=auto'
 starship init fish | source
